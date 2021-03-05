100f3ced2333e4443643fe6d795a3e3fc385084e
LN <qea033@gmail.com>
2021-03-05 14:40:52 +0530

    ebay project

---
+++ src/test/java/com/ebay/EBayProject/EbayClass.java
@@ -0,0 +1,35 @@
+package com.ebay.EBayProject;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+public class EbayClass {
+	
+	public static WebDriver driver;
+	@BeforeSuite
+	public static void environmentSetup() {
+		System.setProperty("webdriver.chrome.driver","chromedriver.exe");
+		
+		driver = new ChromeDriver();
+		
+		driver.get("http://www.paypal.com");
+	}
+	
+	@Test
+	public void testTitle() {
+		
+		String expectedTitle = "International shopping made easy. This and more with PayPal";
+				
+		String actualTitle = driver.getTitle();
+		
+		Assert.assertEquals(actualTitle, expectedTitle);
+		System.out.println(actualTitle);
+	}
+	
+	public void tearDown() {
+		driver.close();
+	}
+}
